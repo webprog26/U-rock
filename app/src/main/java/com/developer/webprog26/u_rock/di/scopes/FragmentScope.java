9f5a0d4328eec2129e66628111df5cd27966ce9f
webprog26 <webprog26@gmail.com>
2017-09-10 17:02:50 +0300

    Dagger 2 build dependecies and custom scopes added

---
+++ app/src/main/java/com/developer/webprog26/u_rock/di/scopes/FragmentScope.java
@@ -0,0 +1,14 @@
+package com.developer.webprog26.u_rock.di.scopes;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * FragmentScope
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FragmentScope {
+}
