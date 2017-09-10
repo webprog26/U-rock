821347e87028dff196ef7cf485649a1a14d8b223
webprog26 <webprog26@gmail.com>
2017-09-10 16:38:17 +0300

    MainView and MainPresenter interfaces added

---
+++ app/src/main/java/com/developer/webprog26/u_rock/Article.java
@@ -0,0 +1,8 @@
+package com.developer.webprog26.u_rock;
+
+/**
+ * Single article POJO class
+ */
+
+public class Article {
+}
---
+++ app/src/main/java/com/developer/webprog26/u_rock/mvp/MainView.java
@@ -0,0 +1,36 @@
+package com.developer.webprog26.u_rock.mvp;
+
+import com.developer.webprog26.u_rock.Article;
+
+import java.util.ArrayList;
+
+/**
+ * Manages app main {@link android.app.Activity} behaviour
+ */
+
+public interface MainView {
+
+    /**
+     * Sets greetings message into appropriate {@link android.widget.TextView}
+     * @param userName {@link String}
+     */
+    void setGreetingsMessage(final String userName);
+
+    /**
+     * Sets new articles message
+     * @param newArticlesNumber int
+     */
+    void setNewArticlesMessage(final int newArticlesNumber);
+
+    /**
+     * Sets new comments message if curent user is subscribed to any
+     * @param newCommentsNumber int
+     */
+    void setNewCommentsMessage(final int newCommentsNumber);
+
+    /**
+     * Updates visible to user list of existing articles on the main screen
+     * @param articles {@link Article}
+     */
+    void updateArticlesListOnTheMainScreen(ArrayList<Article> articles);
+}
