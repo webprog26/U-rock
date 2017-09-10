71d486f23c7020ce18e4a9b9d5db5b5734876019
Volodymyr Shalashenko <webprog26@gmail.com>
2017-09-10 16:39:30 +0300

    MainView and MainPresenter interfaces added (#1)

--- app/src/main/java/com/developer/webprog26/u_rock/MainActivity.java
+++ app/src/main/java/com/developer/webprog26/u_rock/MainActivity.java
@@ -3,11 +3,35 @@ package com.developer.webprog26.u_rock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.developer.webprog26.u_rock.mvp.MainView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements MainView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void setGreetingsMessage(String userName) {
+
+    }
+
+    @Override
+    public void setNewArticlesMessage(int newArticlesNumber) {
+
+    }
+
+    @Override
+    public void setNewCommentsMessage(int newCommentsNumber) {
+
+    }
+
+    @Override
+    public void updateArticlesListOnTheMainScreen(ArrayList<Article> articles) {
+
     }
 }
