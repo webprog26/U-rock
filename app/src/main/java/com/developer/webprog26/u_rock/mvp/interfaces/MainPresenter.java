738eced5619e2761b487a0d7b8043a113b901670
Volodymyr Shalashenko <webprog26@gmail.com>
2017-09-11 08:57:29 +0300

    Dagger2 dependecies and custom scopes added (#3)
    * MainView and MainPresenter interfaces added (#1)
    
    * Dagger 2 build dependecies and custom scopes added (#2)

---
+++ app/src/main/java/com/developer/webprog26/u_rock/mvp/interfaces/MainPresenter.java
@@ -0,0 +1,52 @@
+package com.developer.webprog26.u_rock.mvp.interfaces;
+
+/**
+ * Manages app main screen actions
+ */
+
+public interface MainPresenter {
+
+    /**
+     * Executes user search
+     * @param userSearchRequest {@link String}
+     */
+    void executeUserSearch(final String userSearchRequest);
+
+    /**
+     * Notifies {@link MainView} to set user greetings message
+     * @param userName {@link String}
+     */
+    void notifyMainViewToSetUserGreetingsMessage(final String userName);
+
+    /**
+     * Notifies {@link MainView} to set user greetings message
+     * @param newArticlesNumber int
+     */
+    void notifyMainViewToSetNewArticlesMessage(final int newArticlesNumber);
+
+    /**
+     * Notifies {@link MainView} to set user greetings message
+     * @param newCommentsNumber int
+     */
+    void notifyMainViewToSetNewCommentsMessage(final int newCommentsNumber);
+
+    /**
+     * Switches app screen to home
+     */
+    void goToHomeScreen();
+
+    /**
+     * Switches app screen to popular
+     */
+    void goToPopularScreen();
+
+    /**
+     * Switches app screen to recommended
+     */
+    void goToRecommendedScreen();
+
+    /**
+     * Switches app screen to favourites
+     */
+    void goToFavouritesScreen();
+}
