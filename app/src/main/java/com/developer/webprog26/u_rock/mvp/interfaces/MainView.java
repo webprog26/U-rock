831caa0efeb3519af7eed3c168c8d39409de562e
webprog26 <webprog26@gmail.com>
2017-09-16 13:58:58 +0300

    BottomNavigationView selection depending on start fragment index added

--- app/src/main/java/com/developer/webprog26/u_rock/mvp/interfaces/MainView.java
+++ app/src/main/java/com/developer/webprog26/u_rock/mvp/interfaces/MainView.java
@@ -2,6 +2,7 @@ package com.developer.webprog26.u_rock.mvp.interfaces;
 
 import android.support.annotation.IdRes;
 import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 
 /**
@@ -15,4 +16,7 @@ public interface MainView {
 
     @NonNull
     FragmentManager getScreenFragmentManager();
+
+    @NonNull
+    BottomNavigationView getBottomNavigationView();
 }
